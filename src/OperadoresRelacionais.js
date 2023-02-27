907838b97ffc145d4c7cd30c42e1d790f9471a72
Gabriel Ferraz <gcdsfff7@gmail.com>
2023-02-27 22:02:31 +0000

    Update 2 files

---
+++ src/OperadoresRelacionais.js
@@ -0,0 +1,20 @@
+/* Aqui estamos fazendo uma comparação de igualdade de
+valores */
+console.log('(01)', '1' == 1)
+/* Aqui estamos testando se os números são estritamente
+iguais, ou seja, se o valor e o tipo são os mesmos */
+console.log('(02)', '1' === 1)
+/* Aqui estamos fazendo uma comparação por diferença de
+valores */
+console.log('(03)', '1' != 1)
+/* Aqui estamos vendo se os números são estritamente
+diferentes, ou seja, além do valor também do tipo */
+console.log('(04)', '3' !== 3)
+/* Aqui vamos comparar se um número é maior que outro */
+console.log('(05)', 5 > 1)
+/* Aqui vamos comparar se um número é menor que outro */
+console.log('(06)', 5 < 1)
+/* Aqui vamos comparar se um número é maior que outro ou igual */
+console.log('(07)', 5 >= 1)
+/* Aqui vamos comparar se um número é menor que outro ou igual */
+console.log('(08)', 5 <= 1)
